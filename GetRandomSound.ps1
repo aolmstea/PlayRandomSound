a6877f11f197ee38de16ac8f33ae3275e816a44c
aolmstea <alexolmstead92@gmail.com>
2023-04-23 13:40:06 -0700

    Add files via upload

---
+++ GetRandomSound.ps1
@@ -0,0 +1,5 @@
+﻿Add-Type -AssemblyName presentationCore
+$Sound = Get-ChildItem C:\Users\alexa\Documents\Scripts\sounds -Recurse | Where-Object {$_.extension -eq ".wav"} | Select-Object -ExpandProperty FullName 
+$RandomSound = $Sound | Get-Random -Count 1 
+
+& 'C:\Program Files\VideoLAN\VLC\vlc.exe' --qt-start-minimized --play-and-exit --qt-notification=0 $RandomSound
---
+++ random.bat
@@ -0,0 +1,1 @@
+Powershell.exe -executionpolicy remotesigned -File  "C:\Users\alexa\Documents\Scripts\sounds\GetRandomSound.ps1"
